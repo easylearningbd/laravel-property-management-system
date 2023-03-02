b0b0906a545df65933cd968e43603766578b5a0a
easylearningbd <elebdsoft@gmail.com>
2023-03-03 02:05:21 +0600

    12 Dashboard Page Segmentation

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,4 @@
+  <footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
+        <p class="text-muted mb-1 mb-md-0">Copyright © 2022 <a href="https://easylearningbd.com" target="_blank">EasyLearning</a>.</p>
+        <p class="text-muted">EasyLearning With <i class="mb-1 text-primary ms-1 icon-sm" data-feather="heart"></i></p>
+      </footer>
