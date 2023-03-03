a3ed2ebd33710a38a3c167d16a782d4151ffb92e
easylearningbd <elebdsoft@gmail.com>
2023-03-04 02:18:18 +0600

    18 Admin Profile & Image Update Part 2

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -1,7 +1,103 @@
 @extends('admin.admin_dashboard')
 @section('admin')
 
+<div class="page-content">
 
+      
+        <div class="row profile-body">
+          <!-- left wrapper start -->
+          <div class="d-none d-md-block col-md-4 col-xl-4 left-wrapper">
+            <div class="card rounded">
+              <div class="card-body">
+                <div class="d-flex align-items-center justify-content-between mb-2">
+                   
+
+                  <div>
+  <img class="wd-100 rounded-circle" src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" alt="profile">
+                    <span class="h4 ms-3 ">{{ $profileData->username }}</span>
+                  </div>
+
+
+                
+                </div>
+                
+                <div class="mt-3">
+                  <label class="tx-11 fw-bolder mb-0 text-uppercase">Name:</label>
+                  <p class="text-muted">{{ $profileData->name }}</p>
+                </div>
+                <div class="mt-3">
+                  <label class="tx-11 fw-bolder mb-0 text-uppercase">Email:</label>
+                  <p class="text-muted">{{ $profileData->email }}</p>
+                </div>
+                <div class="mt-3">
+                  <label class="tx-11 fw-bolder mb-0 text-uppercase">Phone:</label>
+                  <p class="text-muted">{{ $profileData->phone }}</p>
+                </div>
+                <div class="mt-3">
+                  <label class="tx-11 fw-bolder mb-0 text-uppercase">Address:</label>
+                  <p class="text-muted">{{ $profileData->address }}</p>
+                </div>
+                <div class="mt-3 d-flex social-links">
+                  <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
+                    <i data-feather="github"></i>
+                  </a>
+                  <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
+                    <i data-feather="twitter"></i>
+                  </a>
+                  <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
+                    <i data-feather="instagram"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- left wrapper end -->
+          <!-- middle wrapper start -->
+          <div class="col-md-8 col-xl-8 middle-wrapper">
+            <div class="row">
+             <div class="card">
+              <div class="card-body">
+
+								<h6 class="card-title">Basic Form</h6>
+
+								<form class="forms-sample">
+									<div class="mb-3">
+										<label for="exampleInputUsername1" class="form-label">Username</label>
+										<input type="text" class="form-control" id="exampleInputUsername1" autocomplete="off" placeholder="Username">
+									</div>
+									<div class="mb-3">
+										<label for="exampleInputEmail1" class="form-label">Email address</label>
+										<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+									</div>
+									<div class="mb-3">
+										<label for="exampleInputPassword1" class="form-label">Password</label>
+										<input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password">
+									</div>
+									<div class="form-check mb-3">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+										<label class="form-check-label" for="exampleCheck1">
+											Remember me
+										</label>
+									</div>
+									<button type="submit" class="btn btn-primary me-2">Submit</button>
+									<button class="btn btn-secondary">Cancel</button>
+								</form>
+
+              </div>
+            </div>
+
+
+
+
+            </div>
+          </div>
+          <!-- middle wrapper end -->
+          <!-- right wrapper start -->
+         
+          <!-- right wrapper end -->
+        </div>
+
+			</div>
 
 
 
