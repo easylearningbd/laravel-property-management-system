6bcd9528c69bd8aa4c7c35c88dd06b0061fef935
easylearningbd <elebdsoft@gmail.com>
2023-03-07 01:04:23 +0600

    38 Property Type Crud Part 2

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -17,6 +17,10 @@
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
   <!-- End fonts -->
 
+<!-- Plugin css for this page -->
+  <link rel="stylesheet" href="{{ asset('backend/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.css') }}">
+  <!-- End plugin css for this page -->
+
   <!-- core:css -->
   <link rel="stylesheet" href="{{ asset('backend/assets/vendors/core/core.css') }}">
   <!-- endinject -->
@@ -107,6 +111,11 @@
  @endif 
 </script>
 
+  <!-- Start datatables -->
+  <script src="{{ asset('backend/assets/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+  <script src="{{ asset('backend/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.js') }}"></script> 
+  <script src="{{ asset('backend/assets/js/data-table.js') }}"></script>
+  <!-- End datatables -->
 
 </body>
 </html>    
--- resources/views/backend/type/all_type.blade.php
+++ resources/views/backend/type/all_type.blade.php
@@ -2,7 +2,51 @@
 @section('admin')
 
 
+<div class="page-content">
 
+				<nav class="page-breadcrumb">
+					<ol class="breadcrumb">
+	  <a href="" class="btn btn-inverse-info"> Add Property Type  </a>
+					</ol>
+				</nav>
+
+				<div class="row">
+					<div class="col-md-12 grid-margin stretch-card">
+            <div class="card">
+              <div class="card-body">
+                <h6 class="card-title">Property Type All </h6>
+               
+                <div class="table-responsive">
+                  <table id="dataTableExample" class="table">
+                    <thead>
+                      <tr>
+                        <th>Sl </th>
+                        <th>Type Name </th>
+                        <th>Type Icon </th>
+                        <th>Action </th> 
+                      </tr>
+                    </thead>
+                    <tbody>
+                   @foreach($types as $key => $item)
+                      <tr>
+                        <td>{{ $key+1 }}</td>
+                        <td>{{ $item->type_name }}</td>
+                        <td>{{ $item->type_icon }}</td>
+                        <td>
+       <a href="" class="btn btn-inverse-warning"> Edit </a>
+       <a href="" class="btn btn-inverse-danger"> Delete  </a>
+                        </td> 
+                      </tr>
+                     @endforeach
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+					</div>
+				</div>
+
+			</div>
 
 
 
