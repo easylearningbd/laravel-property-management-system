6853585353e6ec15cc20c8639b78b7b211c4bbf9
easylearningbd <elebdsoft@gmail.com>
2023-03-11 02:04:41 +0600

    64 Manage Property Update Single & Multiple Image Part 1

--- resources/views/backend/property/edit_property.blade.php
+++ resources/views/backend/property/edit_property.blade.php
@@ -278,15 +278,53 @@
 			</div>
  
 
+   <!--  /// Property Main Thambnail Image Update //// -->
+
+<div class="page-content" style="margin-top: -35px;" > 
+       
+        <div class="row profile-body"> 
+          <div class="col-md-12 col-xl-12 middle-wrapper">
+            <div class="row">
+       
+       <div class="card">
+    <div class="card-body">
+        <h6 class="card-title">Edit Main Thambnail Image </h6>
+            
+
+            <form method="post" action="{{ route('update.property') }}" id="myForm" enctype="multipart/form-data">
+                @csrf
+
+        <input type="hidden" name="id" value="{{ $property->id }}">
+        <input type="hidden" name="old_img" value="{{ $property->property_thambnail }}" >
+
+         <div class="row mb-3">
+            <div class="form-group col-md-6">
+                <label class="form-label">Main Thambnail </label>
+                <input type="file" name="property_thambnail" class="form-control" onChange="mainThamUrl(this)"  >
+
+                <img src="" id="mainThmb">
+
+            </div>
 
 
+               <div class="form-group col-md-6">
+                <label class="form-label">  </label> 
+                <img src="{{ asset($property->property_thambnail) }}" style="width:100px; height:100px;">
+            </div>
+        </div><!-- Col -->
+
+  <button type="submit" class="btn btn-primary">Save Changes </button>
 
 
+            </form> 
+        </div>
+          </div>
 
-
-
-
-
+        </div>
+    </div>
+  </div>
+</div> 
+  <!--    /// End  Property Main Thambnail Image Update //// -->
 
  
 
