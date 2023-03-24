9e9d9da9ff9bbab10ca7805e6938757108eb3f58
easylearningbd <elebdsoft@gmail.com>
2023-03-25 02:13:31 +0600

    133 Dispaly Agents Page in Frontend

--- resources/views/frontend/home/team.blade.php
+++ resources/views/frontend/home/team.blade.php
@@ -1,3 +1,7 @@
+ @php
+$agents = App\Models\User::where('status','active')->where('role','agent')->orderBy('id','DESC')->limit(5)->get();
+ @endphp
+
  <section class="team-section sec-pad centred bg-color-1">
             <div class="pattern-layer" style="background-image: url({{ asset('frontend/assets/images/shape/shape-1.png') }});"></div>
             <div class="auto-container">
@@ -6,86 +10,27 @@
                     <h2>Meet Our Excellent Agents</h2>
                 </div>
                 <div class="single-item-carousel owl-carousel owl-theme owl-dots-none nav-style-one">
-                    <div class="team-block-one">
-                        <div class="inner-box">
-                            <figure class="image-box"><img src="{{ asset('frontend/assets/images/team/team-1.jpg') }}" alt=""></figure>
-                            <div class="lower-content">
-                                <div class="inner">
-                                    <h4><a href="agents-details.html">Merrie Lewis</a></h4>
-                                    <span class="designation">Senior Agent</span>
-                                    <ul class="social-links clearfix">
-                                        <li><a href="index.html"><i class="fab fa-facebook-f"></i></a></li>
-                                        <li><a href="index.html"><i class="fab fa-twitter"></i></a></li>
-                                        <li><a href="index.html"><i class="fab fa-google-plus-g"></i></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="team-block-one">
-                        <div class="inner-box">
-                            <figure class="image-box"><img src="{{ asset('frontend/assets/images/team/team-2.jpg') }}" alt=""></figure>
-                            <div class="lower-content">
-                                <div class="inner">
-                                    <h4><a href="agents-details.html">Parks Missie</a></h4>
-                                    <span class="designation">Senior Agent</span>
-                                    <ul class="social-links clearfix">
-                                        <li><a href="index.html"><i class="fab fa-facebook-f"></i></a></li>
-                                        <li><a href="index.html"><i class="fab fa-twitter"></i></a></li>
-                                        <li><a href="index.html"><i class="fab fa-google-plus-g"></i></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="team-block-one">
-                        <div class="inner-box">
-                            <figure class="image-box"><img src="{{ asset('frontend/assets/images/team/team-3.jpg') }}" alt=""></figure>
-                            <div class="lower-content">
-                                <div class="inner">
-                                    <h4><a href="agents-details.html">Mariana Buenos</a></h4>
-                                    <span class="designation">Senior Agent</span>
-                                    <ul class="social-links clearfix">
-                                        <li><a href="index.html"><i class="fab fa-facebook-f"></i></a></li>
-                                        <li><a href="index.html"><i class="fab fa-twitter"></i></a></li>
-                                        <li><a href="index.html"><i class="fab fa-google-plus-g"></i></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="team-block-one">
-                        <div class="inner-box">
-                            <figure class="image-box"><img src="{{ asset('frontend/assets/images/team/team-4.jpg') }}" alt=""></figure>
-                            <div class="lower-content">
-                                <div class="inner">
-                                    <h4><a href="agents-details.html">Stephen Fowler</a></h4>
-                                    <span class="designation">Senior Agent</span>
-                                    <ul class="social-links clearfix">
-                                        <li><a href="index.html"><i class="fab fa-facebook-f"></i></a></li>
-                                        <li><a href="index.html"><i class="fab fa-twitter"></i></a></li>
-                                        <li><a href="index.html"><i class="fab fa-google-plus-g"></i></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="team-block-one">
-                        <div class="inner-box">
-                            <figure class="image-box"><img src="{{ asset('frontend/assets/images/team/team-5.jpg') }}" alt=""></figure>
-                            <div class="lower-content">
-                                <div class="inner">
-                                    <h4><a href="agents-details.html">Daisy Phillips</a></h4>
-                                    <span class="designation">Senior Agent</span>
-                                    <ul class="social-links clearfix">
-                                        <li><a href="index.html"><i class="fab fa-facebook-f"></i></a></li>
-                                        <li><a href="index.html"><i class="fab fa-twitter"></i></a></li>
-                                        <li><a href="index.html"><i class="fab fa-google-plus-g"></i></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    
+
+  @foreach($agents as $item)
+    <div class="team-block-one">
+        <div class="inner-box">
+            <figure class="image-box"><img src="{{ (!empty($item->photo)) ? url('upload/agent_images/'.$item->photo) : url('upload/no_image.jpg') }}" alt="" style="width:370px; height:370px;" ></figure>
+            <div class="lower-content">
+                <div class="inner">
+                    <h4><a href="agents-details.html">{{ $item->name }}</a></h4>
+                    <span class="designation">{{ $item->email }}</span>
+                    <ul class="social-links clearfix">
+                        <li><a href="index.html"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a href="index.html"><i class="fab fa-twitter"></i></a></li>
+                        <li><a href="index.html"><i class="fab fa-google-plus-g"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+     @endforeach               
+                     
                 </div>
             </div>
         </section>
