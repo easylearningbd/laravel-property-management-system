9236822489fb7175a55455b6bb1a0cd43108a2d2
easylearningbd <elebdsoft@gmail.com>
2023-03-27 03:05:14 +0600

    154 Property Location Area In Frontend Part 2

--- resources/views/frontend/home/place.blade.php
+++ resources/views/frontend/home/place.blade.php
@@ -2,6 +2,17 @@
     $skip_state_0 = App\Models\State::skip(0)->first();
     $property_0 = App\Models\Property::where('state',$skip_state_0->id)->get();
 
+    $skip_state_1 = App\Models\State::skip(1)->first();
+    $property_1 = App\Models\Property::where('state',$skip_state_1->id)->get();
+
+     $skip_state_2 = App\Models\State::skip(2)->first();
+    $property_2 = App\Models\Property::where('state',$skip_state_2->id)->get();
+
+    $skip_state_3 = App\Models\State::skip(3)->first();
+    $property_3 = App\Models\Property::where('state',$skip_state_3->id)->get();
+
+
+
  @endphp
 
 
@@ -30,43 +41,43 @@
 
 
 
-                        <div class="col-lg-4 col-md-6 col-sm-12 masonry-item small-column all brand illustration print software logo">
-                            <div class="place-block-one">
-                                <div class="inner-box">
-                                    <figure class="image-box"><img src="{{ asset('frontend/assets/images/resource/place-2.jpg') }}" alt=""></figure>
-                                    <div class="text">
-                                        <h4><a href="categories.html">San Francisco</a></h4>
-                                        <p>08 Properties</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+    <div class="col-lg-4 col-md-6 col-sm-12 masonry-item small-column all brand illustration print software logo">
+        <div class="place-block-one">
+            <div class="inner-box">
+                <figure class="image-box"><img src="{{ asset($skip_state_1->state_image) }}" alt="" style="width:370px; height:275px;"></figure>
+                <div class="text">
+                    <h4><a href="categories.html">{{ $skip_state_1->state_name }}</a></h4>
+                    <p>{{ count($property_1) }} Properties</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
-                        <div class="col-lg-4 col-md-6 col-sm-12 masonry-item small-column all illustration marketing logo">
-                            <div class="place-block-one">
-                                <div class="inner-box">
-                                    <figure class="image-box"><img src="{{ asset('frontend/assets/images/resource/place-3.jpg') }}" alt=""></figure>
-                                    <div class="text">
-                                        <h4><a href="categories.html">Las Vegas</a></h4>
-                                        <p>29 Properties</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+    <div class="col-lg-4 col-md-6 col-sm-12 masonry-item small-column all illustration marketing logo">
+        <div class="place-block-one">
+            <div class="inner-box">
+       <figure class="image-box"><img src="{{ asset($skip_state_2->state_image) }}" alt="" style="width:370px; height:275px;"></figure>
+                <div class="text">
+                    <h4><a href="categories.html">{{ $skip_state_2->state_name }}</a></h4>
+                    <p>{{ count($property_2) }} Properties</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
-                        <div class="col-lg-8 col-md-6 col-sm-12 masonry-item small-column all brand marketing print software">
-                            <div class="place-block-one">
-                                <div class="inner-box">
-                                    <figure class="image-box"><img src="{{ asset('frontend/assets/images/resource/place-4.jpg') }}" alt=""></figure>
-                                    <div class="text">
-                                        <h4><a href="categories.html">New York City</a></h4>
-                                        <p>05 Properties</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+    <div class="col-lg-8 col-md-6 col-sm-12 masonry-item small-column all brand marketing print software">
+        <div class="place-block-one">
+            <div class="inner-box">
+                <figure class="image-box"><img src="{{ asset($skip_state_3->state_image) }}" alt="" style="width:770px; height:275px;"></figure>
+                <div class="text">
+                    <h4><a href="categories.html">{{ $skip_state_3->state_name }}</a></h4>
+                    <p>{{ count($property_3) }} Properties</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
