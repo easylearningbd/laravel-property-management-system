f76e92b88a18345cf9e62c329acc108d93016367
easylearningbd <elebdsoft@gmail.com>
2023-04-02 00:43:58 +0600

    172 Show Blog in frontend Page Part 1

--- resources/views/frontend/home/news.blade.php
+++ resources/views/frontend/home/news.blade.php
@@ -1,3 +1,8 @@
+ @php
+
+ $blog = App\Models\BlogPost::latest()->limit(3)->get();
+ @endphp
+
  <section class="news-section sec-pad">
             <div class="auto-container">
                 <div class="sec-title centred">
@@ -6,84 +11,40 @@
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing sed do eiusmod tempor incididunt <br />labore dolore magna aliqua enim.</p>
                 </div>
                 <div class="row clearfix">
-                    <div class="col-lg-4 col-md-6 col-sm-12 news-block">
-                        <div class="news-block-one wow fadeInUp animated" data-wow-delay="00ms" data-wow-duration="1500ms">
-                            <div class="inner-box">
-                                <div class="image-box">
-                                    <figure class="image"><a href="blog-details.html"><img src="{{ asset('frontend/assets/images/news/news-1.jpg') }}" alt=""></a></figure>
-                                    <span class="category">Featured</span>
-                                </div>
-                                <div class="lower-content">
-                                    <h4><a href="blog-details.html">Including Animation In Your Design System</a></h4>
-                                    <ul class="post-info clearfix">
-                                        <li class="author-box">
-                                            <figure class="author-thumb"><img src="{{ asset('frontend/assets/images/news/author-1.jpg') }}" alt=""></figure>
-                                            <h5><a href="blog-details.html">Eva Green</a></h5>
-                                        </li>
-                                        <li>April 10, 2020</li>
-                                    </ul>
-                                    <div class="text">
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing sed.</p>
-                                    </div>
-                                    <div class="btn-box">
-                                        <a href="blog-details.html" class="theme-btn btn-two">See Details</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-12 news-block">
-                        <div class="news-block-one wow fadeInUp animated" data-wow-delay="300ms" data-wow-duration="1500ms">
-                            <div class="inner-box">
-                                <div class="image-box">
-                                    <figure class="image"><a href="blog-details.html"><img src="{{ asset('frontend/assets/images/news/news-2.jpg') }}" alt=""></a></figure>
-                                    <span class="category">Featured</span>
-                                </div>
-                                <div class="lower-content">
-                                    <h4><a href="blog-details.html">Taking The Pattern Library To The Next Level</a></h4>
-                                    <ul class="post-info clearfix">
-                                        <li class="author-box">
-                                            <figure class="author-thumb"><img src="{{ asset('frontend/assets/images/news/author-2.jpg') }}" alt=""></figure>
-                                            <h5><a href="blog-details.html">George Clooney</a></h5>
-                                        </li>
-                                        <li>April 09, 2020</li>
-                                    </ul>
-                                    <div class="text">
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing sed.</p>
-                                    </div>
-                                    <div class="btn-box">
-                                        <a href="blog-details.html" class="theme-btn btn-two">See Details</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-12 news-block">
-                        <div class="news-block-one wow fadeInUp animated" data-wow-delay="600ms" data-wow-duration="1500ms">
-                            <div class="inner-box">
-                                <div class="image-box">
-                                    <figure class="image"><a href="blog-details.html"><img src="{{ asset('frontend/assets/images/news/news-3.jpg') }}" alt=""></a></figure>
-                                    <span class="category">Featured</span>
-                                </div>
-                                <div class="lower-content">
-                                    <h4><a href="blog-details.html">How New Font Technologies Will Improve The Web</a></h4>
-                                    <ul class="post-info clearfix">
-                                        <li class="author-box">
-                                            <figure class="author-thumb"><img src="{{ asset('frontend/assets/images/news/author-3.jpg') }}" alt=""></figure>
-                                            <h5><a href="blog-details.html">Simon Baker</a></h5>
-                                        </li>
-                                        <li>April 28, 2020</li>
-                                    </ul>
-                                    <div class="text">
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing sed.</p>
-                                    </div>
-                                    <div class="btn-box">
-                                        <a href="blog-details.html" class="theme-btn btn-two">See Details</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    
+@foreach($blog as $item) 
+<div class="col-lg-4 col-md-6 col-sm-12 news-block">
+    <div class="news-block-one wow fadeInUp animated" data-wow-delay="00ms" data-wow-duration="1500ms">
+        <div class="inner-box">
+            <div class="image-box">
+                <figure class="image"><a href="blog-details.html"><img src="{{ asset($item->post_image) }}" alt=""></a></figure>
+                <span class="category">New</span>
+            </div>
+            <div class="lower-content">
+                <h4><a href="blog-details.html">{{ $item->post_title }}</a></h4>
+                <ul class="post-info clearfix">
+                    <li class="author-box">
+                        <figure class="author-thumb"><img src="{{ (!empty($item->user->photo)) ? url('upload/admin_images/'.$item->user->photo) : url('upload/no_image.jpg') }}" alt=""></figure>
+                        <h5><a href=" ">{{ $item['user']['name'] }}</a></h5>
+                    </li>
+                    <li>{{ $item->created_at->format('M d Y') }}</li>
+                </ul>
+                <div class="text">
+                    <p> {{ $item->short_descp }}</p>
+                </div>
+                <div class="btn-box">
+                    <a href="blog-details.html" class="theme-btn btn-two">See Details</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+     @endforeach               
+
+
+
+
+
                 </div>
             </div>
         </section>
