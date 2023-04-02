5036fd7c0d83dcdf196b40af02204344abf08df4
easylearningbd <elebdsoft@gmail.com>
2023-04-03 02:56:24 +0600

    181 Comment Function Backend With Repaly  Part 3

--- resources/views/frontend/blog/blog_details.blade.php
+++ resources/views/frontend/blog/blog_details.blade.php
@@ -56,56 +56,59 @@
     </div>
 </div>
 </div>
+
+@php
+ $comment = App\Models\Comment::where('post_id',$blog->id)->where('parent_id',null)->limit(5)->get();
+@endphp
+
+
 <div class="comments-area">
     <div class="group-title">
         <h4>3 Comments</h4>
     </div>
     <div class="comment-box">
+
+    	@foreach($comment as $com)
         <div class="comment">
             <figure class="thumb-box">
-                <img src="assets/images/news/comment-1.jpg" alt="">
+                <img src="{{ (!empty($com->user->photo)) ? url('upload/user_images/'.$com->user->photo) : url('upload/no_image.jpg') }}" alt="">
             </figure>
             <div class="comment-inner">
                 <div class="comment-info clearfix">
-                    <h5>Rebeka Dawson</h5>
-                    <span>April 10, 2020</span>
+                    <h5>{{ $com->user->name }}</h5>
+                    <span>{{ $com->created_at->format('M d Y') }}</span>
                 </div>
                 <div class="text">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nos trud exerc.</p>
+                	<p>{{ $com->subject  }}</p>
+                    <p>{{ $com->message  }}</p>
                     <a href="blog-details.html"><i class="fas fa-share"></i>Reply</a>
                 </div>
             </div>
         </div>
+
+        @php
+       $reply = App\Models\Comment::where('parent_id',$com->id)->get();
+        @endphp
+
+        @foreach($reply as $rep)
         <div class="comment replay-comment">
             <figure class="thumb-box">
-                <img src="assets/images/news/comment-2.jpg" alt="">
+                <img src="{{ url('upload/ariyan.jpg') }}" alt="">
             </figure>
             <div class="comment-inner">
                 <div class="comment-info clearfix">
-                    <h5>Elizabeth Winstead</h5>
-                    <span>April 10, 2020</span>
+                    <h5>{{ $rep->subject }}</h5>
+                    <span>{{ $rep->created_at->format('M d Y') }}</span>
                 </div>
                 <div class="text">
-                    <p>Lorem ipsum dolor sit amet, consectur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nos</p>
+                    <p>{{ $rep->message }}</p>
                     <a href="blog-details.html"><i class="fas fa-share"></i>Reply</a>
                 </div>
             </div>
         </div>
-        <div class="comment">
-            <figure class="thumb-box">
-                <img src="assets/images/news/comment-3.jpg" alt="">
-            </figure>
-            <div class="comment-inner">
-                <div class="comment-info clearfix">
-                    <h5>Benedict Cumbatch</h5>
-                    <span>April 10, 2020</span>
-                </div>
-                <div class="text">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nos trud exerc.</p>
-                    <a href="blog-details.html"><i class="fas fa-share"></i>Reply</a>
-                </div>
-            </div>
-        </div>
+          @endforeach
+        @endforeach
+
     </div>
 </div>
 <div class="comments-form-area">
