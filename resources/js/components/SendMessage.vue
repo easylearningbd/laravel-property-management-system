bfc24c7fb4edddbcc999e2ab62b5d99dde5d042b
easylearningbd <elebdsoft@gmail.com>
2023-04-18 00:50:52 +0600

    226 Live Chat Application Part 2

--- resources/js/components/SendMessage.vue
+++ resources/js/components/SendMessage.vue
@@ -1,7 +1,46 @@
-<template>
-
+<template> 
 <div>
-	<h4>This is vue </h4>
+
+	<!-- Button trigger modal -->
+<button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#chat">
+  Chat Now 
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="chat" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title fs-5" id="exampleModalLabel">Chat With </h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+
+      <form>
+      <div class="modal-body">
+       <div class="form-group">
+       	<textarea class="form-control" id="" rows="3" placeholder="Type Your Message">
+       		
+       	</textarea>
+       	
+       </div>  
+
+      </div>
+
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Send Message </button>
+      </div>
+
+       </form>
+
+
+
+    </div>
+  </div>
 </div>
 
+
+
+</div>
 </template>
--- resources/views/frontend/frontend_dashboard.blade.php
+++ resources/views/frontend/frontend_dashboard.blade.php
@@ -102,6 +102,11 @@
     <script src="{{ asset('frontend/assets/js/script.js') }}"></script>
   
 
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+
+
+
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
 <script>
